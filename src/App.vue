3fffd823bcfcd790dced05269faf18459d786326
lauram <l.m.vicente@astralcavestudio.com>
2023-03-22 18:00:18 +0100

    menu and hamburguer menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,45 @@
 <template>
-  <header>
-    header
+  <header id="main-header">
+    <section class="top-nav">
+      <img
+        src="./assets/images/icons/thinkabit-logo.png"
+        alt="thinkabit-logo"
+      />
+      <input id="menu-toggle" type="checkbox" />
+      <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
+      </label>
+      <ul class="menu">
+        <li>Home</li>
+        <li>Our client</li>
+        <li>About us</li>
+        <li>Feedback</li>
+        <li>Contact</li>
+      </ul>
+    </section>
   </header>
-  <main>Main</main>
+  <main>
+    <section class="section-home">
+      <div>
+        <h2> A Digital Product Agency</h2>
+        <div>
+          Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services.
+        </div>
+        <button>
+          Contact Now
+        </button>
+      </div>
+      <div>
+        <img src="./assets/images/people/briefing2.jpg"
+        alt="briefing"
+      />
+      </div>
+     </section>
+    <section class="section-client">Our Client</section>
+    <section class="section-about">How can we help your Business ?</section>
+    <section class="section-feedback">What our happy client say</section>
+    <section class="section-contact">Subscribe Newsletter</section>
+  </main>
   <footer>Footer</footer>
 </template>
 
